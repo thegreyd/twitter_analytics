4f1f62aa3cd5448327537b583e458ad38f09caa4
Siddharth Sharma <sid.sharma0@gmail.com>
2017-11-24 14:22:49 -0500

    fix filter script

--- codes/mongo-to-es.py
+++ codes/mongo-to-es.py
@@ -16,9 +16,8 @@ def main():
     es1 = Elasticsearch()
     ESinfo=(es1.info())
 
-
-    while True:
-        # Pull from mongo and dump into ES using bulk API
+    #delete = es1.indices.delete(index = 'dicdatabase')
+    def upload():
         actions = []
         for data in tqdm(col.find(), total=col.count()):
             data.pop('_id')
@@ -30,7 +29,7 @@ def main():
             }
             actions.append(action)
             actions.append(filter(data))
-        #delete = es1.indices.delete(index = 'dicdatabase')
+        
         request_body = {
             "settings" : {
                 "number_of_shards": 1,
@@ -39,7 +38,8 @@ def main():
         }
         es1.indices.create(index='dicdatabase', body = request_body, ignore=400)
         res = es1.bulk(index = 'dicdatabase', body = actions, refresh = True)
-        time.sleep(20)
+
+    upload()
 
 def filter(data):
     essentials = {
@@ -61,16 +61,17 @@ def filter(data):
             "full_name": None
         }
     }
-    for e in essentials.keys():
-        if isinstance(essentials[e], dict):
-            attrs = essentials[e].keys()
-            for i in attrs:
-                if i in e:
-                    essentials[e][i] = data[e][i]
-        else:
-            if e in data:
-                essentials[e] = data[e]
     
+    for e in essentials.keys():
+        if e in data:
+            if isinstance(essentials[e], dict):
+                attrs = essentials[e].keys()
+                for i in attrs:
+                    if i in data[e]:
+                        essentials[e][i] = data[e][i]
+            else:
+                essentials[e] = data[e]
+        
     return json.dumps(essentials)
     
 if __name__ == "__main__":

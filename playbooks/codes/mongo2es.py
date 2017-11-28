321a9fff7732744fa2952e02ad134f534bdb7685
Siddharth Sharma <sid.sharma0@gmail.com>
2017-11-27 20:09:36 -0500

    update

--- playbooks/codes/mongo2es.py
+++ playbooks/codes/mongo2es.py
@@ -2,39 +2,43 @@ from pymongo import MongoClient
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 from tqdm import tqdm
+import time
 
 def main():
-
     mgclient = MongoClient()
     db = mgclient['dicdatabase']
-    col = db['common_all']
+    col = db['common_latest']
 
     print(col.count())
 
     es1 = Elasticsearch(['10.0.0.105'])
-
+    es2 = Elasticsearch(['10.0.0.49'])
+    
     def upload():
         actions = []
+        
         for data in tqdm(col.find(), total=col.count()):
             action = {
-                    "index": {
-                            "_index": 'common_all',
-                            "_type": 'twitterreddit',
-                            }
+                "index": {
+                    "_index": 'common_latest',
+                    "_type": 'twitterreddit',
+                }
             }
-            data.pop("_id")
             actions.append(action)
             actions.append(data)
-        #print(actions)
-        
+            
         request_body = {
             "settings" : {
                 "number_of_shards": 1,
                 "number_of_replicas": 0
             }
         }
-        es1.indices.create(index='common_all', body = request_body, ignore=400)
-        res = es1.bulk(index = 'common_all', body = actions, refresh = True)
+        
+        es2.indices.delete(index='common_latest', ignore=[400, 404])
+        es2.indices.create(index='common_latest', body = request_body, ignore=400)
+        res2 = es2.bulk(index = 'common_latest', body = actions, refresh = True)
+
+        res1 = es1.bulk(index = 'common_all', body = actions, refresh = True)
 
     upload()
 
